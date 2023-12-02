13bd5488a382b33a7a3fd6328f18171a88832308
Gavin Matthews <gavindmatthews+website@gmail.com>
2023-12-01 18:37:05 -0700

    optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import media from "astro-media/integration";
+
+export default defineConfig({
+  integrations: [
+    // towards the top is better
+    media(),
+  ],
+});
 
 // https://astro.build/config
 export default defineConfig({

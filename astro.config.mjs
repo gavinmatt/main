2ae41d4b06c79dd71fd224a4bfbbc8eb3c92c94d
Gavin Matthews <gavindmatthews+website@gmail.com>
2023-12-01 18:47:19 -0700

    nuke the optimization...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import media from "astro-media/integration";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(),media()]
+  integrations: [mdx(), sitemap(), tailwind(),]
 });

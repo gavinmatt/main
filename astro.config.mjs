292ca0e91daea2b0ef0a66d185f7673d9e09ae48
Gavin Matthews <gavindmatthews+website@gmail.com>
2024-01-25 07:14:52 -0700

    main sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://gavmatt.com',
+  integrations: [mdx(), sitemap(), tailwind(), react(),]
 });

79ba26132578d064e325f7d29ce771c2f213fb0e
Gavin Matthews <gavindmatthews+website@gmail.com>
2024-01-24 21:43:44 -0700

    play width

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -4,7 +4,7 @@ const Temperature = ({ data }) => {
     return (
         <div className="flex flex-wrap justify-center gap-4">
             {data.map((item, index) => (
-                <div key={index} className="border border-gray-400 rounded shadow p-4 text-center w-32 h-32 flex items-center justify-center">
+                <div key={index} className="border border-gray-400 rounded shadow p-4 text-center w-48 h-32 flex items-center justify-center">
                     <div>
                         <p className="font-bold">Temperature</p>
                         <p>{item.lastData.tempf}°F</p>

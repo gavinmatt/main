fada9b23d2b3f0091dc665bc6ae1acb3592412e9
Gavin Matthews <gavindmatthews+website@gmail.com>
2025-11-08 22:38:41 -0700

    Yep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'   // ⬅️ add this
 
-
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://gavmatt.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
-});
-
-
+  output: 'server',                                // ⬅️ critical
+  adapter: vercel(),                               // ⬅️ critical
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+})

7236f2c11f02914435357fc993d67b652223fde9
Gavin Matthews <gavindmatthews+website@gmail.com>
2026-01-28 15:31:22 -0700

    No-node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel/serverless'
-import node from "@astrojs/node";
 import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/astro";
 
 export default defineConfig({

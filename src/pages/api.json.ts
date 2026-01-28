f3c60a39c924f88308dd1584f4123dc6b7e10086
Gavin Matthews <gavindmatthews+website@gmail.com>
2026-01-28 15:37:43 -0700

    JSON

--- src/pages/api.json.ts
+++ src/pages/api.json.ts
@@ -4,26 +4,27 @@ import type { APIRoute } from "astro";
 import aj from "arcjet:client";
 
 export const GET: APIRoute = async ({ request }) => {
-  const decision = await aj.protect(request, { requested: 5 }); // Deduct 5 tokens from the bucket
-  console.log("Arcjet decision", decision);
+  const decision = await aj.protect(request, { requested: 5 });
 
   if (decision.isDenied()) {
     if (decision.reason.isRateLimit()) {
       return Response.json(
-        { error: "Too Many Requests", reason: decision.reason },
+        { error: "Too Many Requests" },
         { status: 429 },
       );
-    } else if (decision.reason.isBot()) {
+    }
+
+    if (decision.reason.isBot()) {
       return Response.json(
-        { error: "No bots allowed", reason: decision.reason },
-        { status: 403 },
-      );
-    } else {
-      return Response.json(
-        { error: "Forbidden", reason: decision.reason },
+        { error: "No bots allowed" },
         { status: 403 },
       );
     }
+
+    return Response.json(
+      { error: "Forbidden" },
+      { status: 403 },
+    );
   }
 
   return Response.json({ message: "Hello world" });

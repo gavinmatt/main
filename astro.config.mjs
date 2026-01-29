e071a112e92727daea835fccd980bf83fb988d6a
Gavin Matthews <gavindmatthews+website@gmail.com>
2026-01-28 18:01:09 -0700

    Revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,40 +3,11 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
-import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/astro";
+import vercel from '@astrojs/vercel/serverless'   // ⬅️ add this
 
 export default defineConfig({
   site: 'https://gavmatt.com',
   output: 'server',                                // ⬅️ critical
   adapter: vercel(),                               // ⬅️ critical
-  integrations: [mdx(), sitemap(), tailwind(), react(), arcjet({
-      rules: [
-        // Shield protects your app from common attacks e.g. SQL injection
-        shield({ mode: "LIVE" }),
-        // Create a bot detection rule
-        detectBot({
-          mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-          // Block all bots except the following
-          allow: [
-            "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-            // Uncomment to allow these other common bot categories
-            // See the full list at https://arcjet.com/bot-list
-            //"CATEGORY:MONITOR", // Uptime monitoring services
-            //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-          ],
-        }),
-        // Create a token bucket rate limit. Other algorithms are supported.
-        tokenBucket({
-          mode: "LIVE",
-          // Tracked by IP address by default, but this can be customized
-          // See https://docs.arcjet.com/fingerprints
-          //characteristics: ["ip.src"],
-          refillRate: 5, // Refill 5 tokens per interval
-          interval: 10, // Refill every 10 seconds
-          capacity: 10, // Bucket capacity of 10 tokens
-        }),
-      ],
-    }),
-  ],
-});
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+})

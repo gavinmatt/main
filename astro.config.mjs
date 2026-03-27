532ce55ebcf521109216f9050ac15c8b22db48f9
Gavin Matthews <gavindmatthews+website@gmail.com>
2026-03-26 20:14:22 -0600

    Fix astro error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'   // ⬅️ add this
+import vercel from '@astrojs/vercel'   // ⬅️ add this
 
 export default defineConfig({
   site: 'https://gavmatt.com',
